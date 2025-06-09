c4e96cd94b458e23c3c963532f5e796b68ed0134
Shailaja <136426263+anshailaja@users.noreply.github.com>
2025-06-09 11:54:03 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,22 @@
+package Virat;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		int math = 98;
+		int sci = 12;
+		int eng = 19;
+		int totalMarks;
+		totalMarks= math+ sci+ eng;
+		float average = (98+12+19)/3;
+		System.out.println(totalMarks);
+		System.out.println(average);
+			
+
+	}
+
+}
+
+//firstly I declare the variable average with datatype float then it gives the output with decimal point and
+//If I declare the variable average with datatype int then it gives the output with integer, so the diff is that 
+//int value store the number without decimal point and the float store the number with decimal point.
