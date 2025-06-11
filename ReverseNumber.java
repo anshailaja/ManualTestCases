e6a1bac48ee9ce6d929983ea6e97862426b84d42
Shailaja <136426263+anshailaja@users.noreply.github.com>
2025-06-11 20:22:10 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -5,6 +5,13 @@ public class ReverseNumber {
 	public static void main(String[] args) {
 		int num=12345;
 		int rev=0;
+		while(num>0) {
+			int digit=num%10;
+			rev=(rev*10)+digit;
+			num=num/10;
+		}
+		System.out.println("The reverse of given number is " + rev);
+		
 		
 		 
 		
